eafb936b63e736204dec5df952f15ca4be465fcd
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2024-02-01 11:19:25 +0100

    Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import Button from '@mui/material/Button';
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<Button variant="contained">Hello world</Button>);
