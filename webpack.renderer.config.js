4bd8eea467cecb289ef8293d1b71d2b13267fb78
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2024-02-01 10:56:05 +0100

    SASS

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,9 +1,9 @@
 const rules = require('./webpack.rules');
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {test: /\.css$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
+  {test: /\.scss$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]},
+);
 
 module.exports = {
   // Put your normal webpack config below here
