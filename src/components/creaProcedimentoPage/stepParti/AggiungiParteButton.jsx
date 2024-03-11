4613d1ece4c4da01110580f5ac92a20224463801
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2024-03-11 19:56:20 +0100

    crea/allega parti

---
+++ src/components/creaProcedimentoPage/stepParti/AggiungiParteButton.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AddIcon from '@mui/icons-material/Add';
+import { useTheme } from '@mui/material/styles';
+
+export default function AggiungiParteButton(props) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const theme = useTheme()
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '85%',
+        height: '90%',
+        bgcolor: 'background.default',
+        border: 'unset',
+        boxShadow: 24,
+        p: 4,
+    };
+    const buttonColor = '#82b9ec'
+    const buttonHoverColor = '#4a769b'
+      
+
+    return(
+        <div style={{display: 'inline-block', ...props.sx}}>
+            <Button 
+                onClick={handleOpen}
+                variant='text'
+                sx={{color: buttonColor, '&:hover, &:hover svg':{backgroundColor: 'unset', color: buttonHoverColor}}}
+                startIcon={<AddIcon sx={{color: buttonColor, transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms'}}/>}
+            >
+                Crea una parte
+            </Button>
+            <Modal
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="keep-mounted-modal-title"
+                aria-describedby="keep-mounted-modal-description"
+            >
+                <Box sx={style}>
+                <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
+                    Creazione parte
+                </Typography>
+                <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
+                   Corpo della creazione parte
+                </Typography>
+                </Box>
+            </Modal>
+        </div>
+    )
+}
--- src/components/creaProcedimentoPage/stepParti/StepParti.jsx
+++ src/components/creaProcedimentoPage/stepParti/StepParti.jsx
@@ -3,6 +3,8 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import TabellaParti from './TabellaParti.jsx';
+import AggiungiParteButton from './AggiungiParteButton.jsx';
+import CollegaParteButton from './CollegaParteButton.jsx';
 
 function StepParti(props, ref){
 
@@ -11,13 +13,14 @@ function StepParti(props, ref){
 
     return(
         <div style={{display: 'flex', flexDirection:'column', alignItems: 'center', justifyContent:'center', rowGap:'4rem', padding: '4.5rem 0'}}>
-            {/* Procedimento di mediazione */}
-            <Grid xs={12}>
+            {/* Lista delle parti */}
+            <Grid container xs={12}>
                 <Grid xs={12} sx={{borderBottom:'1px solid #467bae61', margin: '0 0 0 1rem', width: 'calc(100% - 1rem)'}}><Typography sx={{fontWeight: '400', fontSize: formLabelFontSize, color: '#467bae'}}>Lista delle parti</Typography></Grid>
-                
                 <TabellaParti></TabellaParti>
-
-                
+                <div style={{margin: '0 0 0 1rem', width: 'calc(100% - 1rem)'}}>
+                    <AggiungiParteButton sx={{marginRight: '10rem'}}/>
+                    <CollegaParteButton/>
+                </div>
             </Grid>
         </div>
     )
