0cc94365eb5f3da5efee9ce84cd75b1764f10893
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2024-10-05 01:05:17 +0200

    refactoring package

--- webpack.rules.js
+++ webpack.rules.js
@@ -7,13 +7,7 @@ module.exports = [
   {
     test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
-    use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
-      },
-    },
-  },
+  },  
   {
     test: /\.jsx?$/,
     exclude: /node_modules/,
