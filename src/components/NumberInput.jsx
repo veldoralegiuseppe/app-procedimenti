4dcd8d921495f3af72ebeebf5281f772ff53084e
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2024-10-22 23:55:46 +0200

    feat(NumberInput): aggiunto componente input numerico

---
+++ src/components/NumberInput.jsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import { CssTextField } from '@theme/MainTheme';
+import { InputAdornment, IconButton } from '@mui/material';
+import { ArrowDropUp, ArrowDropDown } from '@mui/icons-material';
+
+export default function NumberInput({
+  value = 0,
+  onChange,
+  min = 0,
+  max = 100,
+}) {
+  const [inputValue, setInputValue] = React.useState(value);
+
+  React.useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  return (
+    <CssTextField
+      //label="Numero di incontri"
+      type="text"
+      variant="outlined"
+      size="small"
+      value={inputValue}
+      onChange={(event) => {
+        const newValue = event.target.value;
+        const sanitizedValue = newValue.replace(/[^0-9]/g, '');
+        if (
+          sanitizedValue === '' ||
+          isNaN(sanitizedValue) ||
+          sanitizedValue < min ||
+          sanitizedValue > max
+        ) {
+          event.target.value = sanitizedValue;
+          return;
+        }
+      }}
+      sx={{
+        width: '5rem',
+        //cursor: 'default',
+        '& .MuiInputBase-input': {
+          cursor: 'default', 
+          minWidth: '1.5rem'
+        },
+      }}
+      InputProps={{
+        readOnly: true,
+
+        endAdornment: (
+          <InputAdornment
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              position: 'relative',
+              left: '2px',
+              bottom: '1px',
+              maxWidth: '2px',
+            }}
+            position="end"
+          >
+            <IconButton
+              onClick={() => {
+                const newValue = inputValue + 1 <= max ? inputValue + 1 : max;
+                setInputValue(newValue);
+                if (onChange) onChange(parseInt(newValue));
+              }}
+              sx={{ padding: 0, transform: ' translateY(-3px)' }}
+            >
+              <ArrowDropUp />
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                const newValue = inputValue - 1 >= min ? inputValue - 1 : min;
+                setInputValue(newValue);
+                if (onChange) onChange(parseInt(newValue));
+              }}
+              sx={{ padding: 0, transform: ' translateY(-6px)' }}
+            >
+              <ArrowDropDown />
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
