6a2cb9ee70b38d088810bb641f285e9a4c36a9ea
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2025-01-13 21:41:53 +0100

    fix: statistiche

--- src/main/database/dao/ProcedimentoDAO.js
+++ src/main/database/dao/ProcedimentoDAO.js
@@ -20,145 +20,121 @@ class ProcedimentoDAO extends BaseDAO {
   }
 
   async calculateStatistics(query = {}) {
-    const pipeline = [
-      // Step 1: Filtra i documenti iniziali
-      { $match: query },
-
-      // Step 2: Raggruppa le spese esterne a livello di documento
+    const pipelineTransazioniGlobali = [
+      {
+        $project: {
+          transazioni: {
+            $filter: {
+              input: { $objectToArray: '$$ROOT' }, // Converte la root in un array di chiavi-valori
+              as: 'field',
+              cond: { $eq: ['$$field.v.type', 'transazione'] }, // Filtra gli oggetti con type = "transazione"
+            },
+          },
+        },
+      },
+      { $unwind: '$transazioni' },
       {
         $group: {
-          _id: null, // Raggruppa tutti i procedimenti
-          totaleCompensoMediatoreDovuto: {
-            $sum: '$compensoMediatore.importoDovuto',
-          },
-          totaleCompensoMediatoreCorrisposto: {
-            $sum: '$compensoMediatore.importoCorrisposto',
-          },
-          totaleSpeseAvvioSedeSecondariaDovuto: {
-            $sum: '$speseAvvioSedeSecondaria.importoDovuto',
-          },
-          totaleSpeseAvvioSedeSecondariaCorrisposto: {
-            $sum: '$speseAvvioSedeSecondaria.importoCorrisposto',
-          },
-          totaleSpeseIndennitaSedeSecondariaDovuto: {
-            $sum: '$speseIndennitaSedeSecondaria.importoDovuto',
-          },
-          totaleSpeseIndennitaSedeSecondariaCorrisposto: {
-            $sum: '$speseIndennitaSedeSecondaria.importoCorrisposto',
-          },
-          persone: { $first: "$persone" },
+          _id: '$transazioni.v.key',
+          transazione: { $mergeObjects: '$transazioni.v' },
+          importoDovuto: { $sum: '$transazioni.v.importoDovuto' },
+          importoCorrisposto: { $sum: '$transazioni.v.importoCorrisposto' },
         },
       },
-
-      // Step 3: Esplodi l'array "persone"
-      { $unwind: '$persone' },
-
-      // Step 4: Calcola il totale delle spese per ogni persona
-      {
-        $addFields: {
-          'persone.totalDovuto': {
-            $add: [
-              '$persone.speseAvvio.importoDovuto',
-              '$persone.spesePostali.importoDovuto',
-              '$persone.speseIndennita.importoDovuto',
-              '$persone.speseMancatoAccordo.importoDovuto',
-              '$persone.spesePositivoPrimoIncontro.importoDovuto',
-              '$persone.spesePositivoOltrePrimoIncontro.importoDovuto',
-            ],
-          },
-          'persone.totalCorrisposto': {
-            $add: [
-              '$persone.speseAvvio.importoCorrisposto',
-              '$persone.spesePostali.importoCorrisposto',
-              '$persone.speseIndennita.importoCorrisposto',
-              '$persone.speseMancatoAccordo.importoCorrisposto',
-              '$persone.spesePositivoPrimoIncontro.importoCorrisposto',
-              '$persone.spesePositivoOltrePrimoIncontro.importoCorrisposto',
-            ],
-          },
-        },
-      },
-
-      // Step 5: Raggruppa i dati per ruolo
-      {
-        $group: {
-          _id: "$persone.ruolo", // Raggruppa per ruolo
-          totalDovuto: { $sum: "$persone.totalDovuto" },
-          totalCorrisposto: { $sum: "$persone.totalCorrisposto" },
-          totaleCompensoMediatoreDovuto: { $first: "$totaleCompensoMediatoreDovuto" },
-          totaleCompensoMediatoreCorrisposto: { $first: "$totaleCompensoMediatoreCorrisposto" },
-          totaleSpeseAvvioSedeSecondariaDovuto: { $first: "$totaleSpeseAvvioSedeSecondariaDovuto" },
-          totaleSpeseAvvioSedeSecondariaCorrisposto: { $first: "$totaleSpeseAvvioSedeSecondariaCorrisposto" },
-          totaleSpeseIndennitaSedeSecondariaDovuto: { $first: "$totaleSpeseIndennitaSedeSecondariaDovuto" },
-          totaleSpeseIndennitaSedeSecondariaCorrisposto: { $first: "$totaleSpeseIndennitaSedeSecondariaCorrisposto" },
-        },
-      },
-
-      // Step 6: Raggruppa i dati globali
-      {
-        $group: {
-          _id: null,
-          incassoPartiDovuto: {
-            $sum: {
-              $cond: [{ $eq: ['$_id', 'PARTE ISTANTE'] }, '$totalDovuto', 0],
-            },
-          },
-          incassoPartiCorrisposto: {
-            $sum: {
-              $cond: [
-                { $eq: ['$_id', 'PARTE ISTANTE'] },
-                '$totalCorrisposto',
-                0,
-              ],
-            },
-          },
-          incassoContropartiDovuto: {
-            $sum: {
-              $cond: [{ $eq: ['$_id', 'CONTROPARTE'] }, '$totalDovuto', 0],
-            },
-          },
-          incassoContropartiCorrisposto: {
-            $sum: {
-              $cond: [{ $eq: ['$_id', 'CONTROPARTE'] }, '$totalCorrisposto', 0],
-            },
-          },
-
-          // Passa direttamente le spese esterne senza risommare
-          totaleCompensoMediatoreDovuto: {
-            $first: '$totaleCompensoMediatoreDovuto',
-          },
-          totaleCompensoMediatoreCorrisposto: {
-            $first: '$totaleCompensoMediatoreCorrisposto',
-          },
-          totaleSpeseAvvioSedeSecondariaDovuto: {
-            $first: '$totaleSpeseAvvioSedeSecondariaDovuto',
-          },
-          totaleSpeseAvvioSedeSecondariaCorrisposto: {
-            $first: '$totaleSpeseAvvioSedeSecondariaCorrisposto',
-          },
-          totaleSpeseIndennitaSedeSecondariaDovuto: {
-            $first: '$totaleSpeseIndennitaSedeSecondariaDovuto',
-          },
-          totaleSpeseIndennitaSedeSecondariaCorrisposto: {
-            $first: '$totaleSpeseIndennitaSedeSecondariaCorrisposto',
-          },
-        },
-      },
-
-      // Step 7: Proietta il risultato finale
       {
         $project: {
           _id: 0,
-          incassoPartiDovuto: 1,
-          incassoPartiCorrisposto: 1,
-          incassoContropartiDovuto: 1,
-          incassoContropartiCorrisposto: 1,
-          totaleCompensoMediatoreDovuto: 1,
-          totaleCompensoMediatoreCorrisposto: 1,
-          totaleSpeseAvvioSedeSecondariaDovuto: 1,
-          totaleSpeseAvvioSedeSecondariaCorrisposto: 1,
-          totaleSpeseIndennitaSedeSecondariaDovuto: 1,
-          totaleSpeseIndennitaSedeSecondariaCorrisposto: 1,
+          transazione: {
+            $mergeObjects: [
+              '$transazione',
+              {
+                importoDovuto: '$importoDovuto',
+                importoCorrisposto: '$importoCorrisposto',
+                owner: null,
+                stato: {
+                  $cond: [
+                    { $eq: ['$importoCorrisposto', '$importoDovuto'] },
+                    'SALDATO',
+                    {
+                      $cond: [
+                        { $eq: ['$importoCorrisposto', 0] },
+                        'DA SALDARE',
+                        'PARZIALMENTE SALDATO',
+                      ],
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
+    ];
+
+    const pipelineTransazioniPersone = [
+      { $unwind: { path: '$persone', preserveNullAndEmptyArrays: true } },
+      {
+        $project: {
+          transazioni: {
+            $filter: {
+              input: { $objectToArray: '$persone' }, // Converte ogni persona in un array di chiavi-valori
+              as: 'field',
+              cond: { $eq: ['$$field.v.type', 'transazione'] }, // Filtra i sotto-oggetti con type = "transazione"
+            },
+          },
+        },
+      },
+      { $unwind: '$transazioni' },
+      {
+        $group: {
+          _id: { key: '$transazioni.v.key', owner: '$persone.type' },
+          transazione: { $mergeObjects: '$transazioni.v' },
+          importoDovuto: { $sum: '$transazioni.v.importoDovuto' },
+          importoCorrisposto: { $sum: '$transazioni.v.importoCorrisposto' },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          transazione: {
+            $mergeObjects: [
+              '$transazione',
+              {
+                importoDovuto: '$importoDovuto',
+                importoCorrisposto: '$importoCorrisposto',
+                owner: null,
+                stato: {
+                  $cond: [
+                    { $eq: ['$importoCorrisposto', '$importoDovuto'] },
+                    'SALDATO',
+                    {
+                      $cond: [
+                        { $eq: ['$importoCorrisposto', 0] },
+                        'DA SALDARE',
+                        'PARZIALMENTE SALDATO',
+                      ],
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
+    ];
+
+    const pipeline = [
+      { $match: query },
+      {
+        $facet: {
+          transazioniGlobali: pipelineTransazioniGlobali,
+          transazioniPersone: pipelineTransazioniPersone,
+        },
+      },
+      {
+        $project: {
+          transazioniGlobali: 1,
+          transazioniPersone: 1,
         },
       },
     ];
